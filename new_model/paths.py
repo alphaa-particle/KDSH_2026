89379d7599cb4ed7a98806ac4438447be884d38b
Ramkrishna Rathore <tspy1947@gmail.com>
2026-01-12 13:09:50 +0530

    Update paths.py

--- new_model/paths.py
+++ new_model/paths.py
@@ -1,7 +1,8 @@
 import os
 
-# This gives the absolute path of the new_model folder
+# Absolute path of new_model/
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATA_DIR = os.path.join(BASE_DIR, "data")
 INDEX_DIR = os.path.join(BASE_DIR, "indexes")
+CKPT_DIR = os.path.join(BASE_DIR, "checkpoints")
