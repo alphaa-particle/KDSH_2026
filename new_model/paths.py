e9f35e8e08de28313894631b5fdd07742a15c11f
Ramkrishna Rathore <tspy1947@gmail.com>
2026-01-12 13:04:10 +0530

    Create paths.py

---
+++ new_model/paths.py
@@ -0,0 +1,7 @@
+import os
+
+# This gives the absolute path of the new_model folder
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATA_DIR = os.path.join(BASE_DIR, "data")
+INDEX_DIR = os.path.join(BASE_DIR, "indexes")
